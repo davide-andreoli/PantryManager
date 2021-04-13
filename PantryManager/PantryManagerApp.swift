bf3b9a08b3494d5ad16a8c644695bf3f6b43b3c5
Davide Andreoli <davide.andreoli@icloud.com>
2021-04-13 20:43:50 +0200

    Initial Commit

---
+++ PantryManager/PantryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryManagerApp.swift
+//  PantryManager
+//
+//  Created by Davide Andreoli on 13/04/21.
+//
+
+import SwiftUI
+
+@main
+struct PantryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
